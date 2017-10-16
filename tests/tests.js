a7401569cc4740dcdb07ee234c5b1667bfc3ae8d
Jag <jag.chadha@gmail.com>
2017-10-16 14:13:38 +0200

    adding failing tests

--- tests/tests.js
+++ tests/tests.js
@@ -1,11 +1,14 @@
 var test = require('tape');
 
 test('positive control', function(t){
-
     t.equal(1,1);
-
     t.end();
 });
 
+
+test('negative control', function(t){
+    t.equal(1,2);
+    t.end();
+});
 
 
